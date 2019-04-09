a1520cb10bdcba08540775e9f7713ed9c5b9f96b
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2019-04-09 06:59:21 +0100

    Major updates

--- R/rob_summary_plot.R
+++ R/rob_summary_plot.R
@@ -1,4 +1,4 @@
-bias_summary_plot <- function(data, tool){
+rob_summary_plot <- function(data, tool, save="FALSE"){
   rob.df <- data.frame(data)
 
   rob.df$Study <- NULL
@@ -15,12 +15,11 @@ bias_summary_plot <- function(data, tool){
                      factor_key=FALSE))
 
   if(tool=="ROB2"){
-
-  mutate(rob.long, domain_renamed=case_when(domain==1~"Bias due to randomisation",
-                                            domain==2~"Bias due to deviations",
-                                            domain==3~"Bias due to missing data",
-                                            domain==4~"Bias due to outcome measurement",
-                                            domain==5~"Bias due to selection of reported result",
+  rob.long<- mutate(rob.long, domain_renamed=case_when(domain=="D1"~"Bias due to randomisation",
+                                            domain=="D2"~"Bias due to deviations",
+                                            domain=="D3"~"Bias due to missing data",
+                                            domain=="D4"~"Bias due to outcome measurement",
+                                            domain=="D5"~"Bias due to selection of reported result",
                                             TRUE ~ domain)  )
 
   rob.long$judgement<-as.factor(rob.long$judgement)
@@ -29,7 +28,7 @@ bias_summary_plot <- function(data, tool){
                                levels(rob.long$judgement)[c(1,3,2)])
 
   plot<-ggplot(data=rob.long)+
-    geom_bar(mapping=aes(x=domain,fill=judgement),
+    geom_bar(mapping=aes(x=domain_renamed,fill=judgement),
              width=0.7,
              position = "fill",
              color="black")+
@@ -46,7 +45,7 @@ bias_summary_plot <- function(data, tool){
     theme(axis.title.x=element_blank(),
           axis.title.y=element_blank(),
           axis.ticks.y=element_blank(),
-          axis.text.y = element_text(size=18, color = "black"),
+          axis.text.y = element_text(size=10, color = "black"),
           axis.line.x = element_line(colour = "black",
                                      size = 0.5, linetype = "solid"),
           legend.position = "bottom",
@@ -61,28 +60,45 @@ bias_summary_plot <- function(data, tool){
   }
 
   if(tool=="ROBINS-I"){
+    rob.long<- mutate(rob.long, domain_renamed=case_when(domain=="D1"~"Bias due to confounding",
+                                                         domain=="D2"~"Bias due to selection of participants",
+                                                         domain=="D3"~"Bias in classification of interventions",
+                                                         domain=="D4"~"Bias due to deviations from intended interventions",
+                                                         domain=="D5"~"Bias due to missing data",
+                                                         domain=="D6"~"Bias in measurement of outcomes",
+                                                         domain=="D7"~"Bias in selection of the reported result",
+                                                         TRUE ~ domain)  )
+
+    rob.long$judgement<-as.factor(rob.long$judgement)
+
+    rob.long$judgement<-factor(rob.long$judgement,
+                               levels(rob.long$judgement)[c(1,2,4,3)])
+
     plot<-ggplot(data=rob.long)+
-      geom_bar(mapping=aes(x=domain,fill=judgement),
+      geom_bar(mapping=aes(x=domain_renamed,fill=judgement),
                width=0.7,
                position = "fill",
                color="black")+
       coord_flip(ylim = c(0,1))+
       guides(fill = guide_legend(reverse = TRUE))+
       scale_fill_manual("Risk of Bias",
-                        labels = c("    Critical risk of bias      ",
-                                   "    High risk of bias          ",
-                                   "    Unclear risk of bias       ",
-                                   "    Low risk of bias  "),
                         values = c(
-                          "Critical" = "#ff0000",
-                          "High" = "#BF0000",
-                          "Unclear" = "#02C100",
-                          "Low" = "#E2DF07"))+
+
+                          "High" = "#fc8d62",
+                          "Some concerns" = "#808080",
+                          "Low" = "#66c2a5",
+                          "Critical" = "#ff0000"),
+
+                        labels = c(
+                          "    Critical risk of bias",
+                          "    High risk of bias  ",
+                          "    Some concerns       ",
+                          "    Low risk of bias          "))+
       scale_y_continuous(labels = scales::percent)+
       theme(axis.title.x=element_blank(),
             axis.title.y=element_blank(),
             axis.ticks.y=element_blank(),
-            axis.text.y = element_text(size=18, color = "black"),
+            axis.text.y = element_text(size=12, color = "black"),
             axis.line.x = element_line(colour = "black",
                                        size = 0.5, linetype = "solid"),
             legend.position = "bottom",
@@ -95,5 +111,11 @@ bias_summary_plot <- function(data, tool){
             legend.key.size = unit(0.75,"cm"),
             legend.text=element_text(size=14))
   }
+
+  if(save=="TRUE"){
+    ggsave(r)
+
+  }
+
   return(plot)
 }
