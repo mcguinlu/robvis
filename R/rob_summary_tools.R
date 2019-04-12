9bcf1f0ad0e6cf14750b17b9e443409a3ddb9011
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2019-04-12 05:02:00 +0100

    Create new function to list the tool formats that can be plotted using rob_summary()

---
+++ R/rob_summary_tools.R
@@ -0,0 +1,10 @@
+#' Produce summary figure of risk of bias assessments.
+#' @description A function to convert standard risk-of-bias output to tidy data .
+#' @return Risk of bias assessment figure in .png output.
+#' @export
+
+rob_summary_tools <- function(){
+  print("ROB2")
+  print("ROBINS-I")
+  print("QUADAS-2")
+}
