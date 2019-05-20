bb7acc5abed73f83b3586f672a686d4fa0b99a7a
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2019-05-20 13:16:32 +0100

    [Travis] Updates to encoding options when reading files for example data

--- R/exampledataprep.R
+++ R/exampledataprep.R
@@ -1,8 +1,8 @@
 # Load datasets
-data_rob1 <- read.csv("data_raw/data_rob1.csv", header = TRUE)
-data_rob2 <- read.csv("data_raw/data_rob2.csv", header = TRUE)
-data_robins <- read.csv("data_raw/data_robins.csv", header = TRUE)
-data_quadas <- read.csv("data_raw/data_quadas.csv", header = TRUE)
+data_rob1 <- read.csv("data_raw/data_rob1.csv", header = TRUE, fileEncoding = "latin1")
+data_rob2 <- read.csv("data_raw/data_rob2.csv", header = TRUE, fileEncoding = "latin1")
+data_robins <- read.csv("data_raw/data_robins.csv", header = TRUE, fileEncoding = "latin1")
+data_quadas <- read.csv("data_raw/data_quadas.csv", header = TRUE, fileEncoding = "latin1")
 
 usethis::use_data(data_rob1, overwrite = TRUE)
 usethis::use_data(data_rob2, overwrite = TRUE)
