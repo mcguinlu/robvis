5ea23df15057a4d8d23ee8f710cc60d85c5989db
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2019-08-09 15:47:02 +0100

    Re-added template for online platform following updates the general ROBINS-I template

--- R/rob_summary.R
+++ R/rob_summary.R
@@ -257,6 +257,133 @@ if (tool == "ROB2") {
                                                    reverse = TRUE))
   }
 
+# ROBINS-I  ONLINE============================================================
+  if (tool == "ROBINS-I") {
+
+    # Define colouring
+    if (length(colour) > 1) {
+      low_colour <- colour[c(1)]
+      concerns_colour <- colour[c(2)]
+      high_colour <- colour[c(3)]
+      critical_colour <- colour[c(4)]
+    }
+    else {
+      if (colour == "colourblind") {
+        low_colour <- "#fef0d9"
+        concerns_colour <- "#fdcc8a"
+        high_colour <- "#fc8d59"
+        critical_colour <- "#d7301f"
+      }
+      if (colour == "cochrane") {
+        low_colour <- "#02C100"
+        concerns_colour <- "#E2DF07"
+        high_colour <- "#BF0000"
+        critical_colour <- "#820000"
+      }
+    }
+
+    data <- data[,grepl("studyId|RBJ_answer",names(data))]
+    data <- data[,which(is.na(data) == FALSE)]
+
+    # Data preprocessing
+    for (i in 2:9) {
+      data[[i]] <- tolower(data[[i]])
+      data[[i]] <- trimws(data[[i]])
+      data[[i]] <- substr(data[[i]], 0, 1)
+    }
+
+    # Define weights if FALSE and check if there is a weight column if TRUE
+    if(weighted == FALSE) {
+      data[,10] <- rep(1,length(nrow(data)))
+    } else {
+      if(NCOL(data) < 10){stop("Column missing (number of columns < 10). Likely that a column detailing weights for each study is missing.")}
+    }
+
+
+    data.tmp <- data
+    names(data.tmp)[2] <- "Bias due to confounding"
+    names(data.tmp)[3] <- "Bias due to selection of participants"
+    names(data.tmp)[4] <- "Bias in classification of interventions"
+    names(data.tmp)[5] <- "Bias due to deviations from intended interventions"
+    names(data.tmp)[6] <- "Bias due to missing data"
+    names(data.tmp)[7] <- "Bias in measurement of outcomes"
+    names(data.tmp)[8] <- "Bias in selection of the reported result"
+    names(data.tmp)[9] <- "Overall risk of bias"
+    names(data.tmp)[10] <- "Weights"
+
+    #Define dataframe based on value of "overall"
+    if (overall == "FALSE") {
+      data.tmp <- data.tmp[, c(2:8, 10)]
+    }
+    else{
+      data.tmp <- data.tmp[, c(2:10)]
+    }
+
+
+    rob.tidy <- suppressWarnings(tidyr::gather(data.tmp,
+                                               domain, judgement,
+                                               -Weights))
+
+    rob.tidy$judgement <- as.factor(rob.tidy$judgement)
+
+    rob.tidy$domain <- as.factor(rob.tidy$domain)
+
+    rob.tidy$domain <- factor(rob.tidy$domain,
+                              levels(rob.tidy$domain)[c(8, 7, 6, 3, 5, 2, 4, 1)])
+
+    rob.tidy$judgement <- factor(rob.tidy$judgement,
+                                 levels(rob.tidy$judgement)[c(1,4,3,2)])
+
+    plot <- ggplot2::ggplot(data = rob.tidy) +
+      ggplot2::geom_bar(
+        mapping = ggplot2::aes(x = domain, fill = judgement, weight = Weights),
+        width = 0.7,
+        position = "fill",
+        color = "black"
+      ) +
+      ggplot2::coord_flip(ylim = c(0, 1)) +
+      ggplot2::scale_fill_manual(
+        "Risk of Bias",
+        values = c(
+          "m" = concerns_colour,
+          "s" = high_colour,
+          "l" = low_colour,
+          "c" = critical_colour
+        ),
+        labels = c(
+          "  Critical risk  ",
+          "  Serious risk  ",
+          "  Moderate risk ",
+          "  Low risk  "
+        )
+      ) +
+      ggplot2::scale_y_continuous(labels = scales::percent) +
+      ggplot2::theme(
+        axis.title.x = ggplot2::element_blank(),
+        axis.title.y = ggplot2::element_blank(),
+        axis.ticks.y = ggplot2::element_blank(),
+        axis.text.y = ggplot2::element_text(size = 10, color = "black"),
+        axis.line.x = ggplot2::element_line(
+          colour = "black",
+          size = 0.5,
+          linetype = "solid"
+        ),
+        legend.position = "bottom",
+        panel.grid.major = ggplot2::element_blank(),
+        panel.grid.minor = ggplot2::element_blank(),
+        panel.background = ggplot2::element_blank(),
+        legend.background = ggplot2::element_rect(linetype = "solid",
+                                                  colour = "black"),
+        legend.title = ggplot2::element_text(size=9, face = "bold"),
+        legend.key.size = ggplot2::unit(0.75, "cm"),
+        legend.text = ggplot2::element_text(size = 8)
+      ) +
+      ggplot2::guides(fill = ggplot2::guide_legend(title = "Risk of bias judgement",
+                                                   title.position = "bottom",
+                                                   title.hjust = 0.5,
+                                                   reverse = TRUE))
+  }
+
 # QUADAS-2 =====================================================================
 
 if (tool == "QUADAS-2") {
