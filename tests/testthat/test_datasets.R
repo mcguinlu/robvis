b41036db4d4468820f21739378818ba1fd906072
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2019-12-12 16:40:59 +0000

    Initial test commits

--- tests/testthat/test_datasets.R
+++ tests/testthat/test_datasets.R
@@ -1,3 +1,36 @@
 context("Check included datasets")
 
-# FOr all 4 datasets
+# For all 4 datasets
+
+test_that("Size of data_rob2", {
+          expect_equal(length(colnames(data_rob2)),8)
+          expect_equal(length(rownames(data_rob2)),9)
+          }
+          )
+
+test_that("Size of data_robins", {
+  expect_equal(length(colnames(data_robins)),10)
+  expect_equal(length(rownames(data_robins)),12)
+}
+)
+
+test_that("Size of data_quadas", {
+  expect_equal(length(colnames(data_quadas)),7)
+  expect_equal(length(rownames(data_quadas)),12)
+}
+)
+
+test_that("Size of rob1", {
+  expect_equal(length(colnames(data_rob1)),10)
+  expect_equal(length(rownames(data_rob1)),9)
+}
+)
+
+
+test_that("Data crosscheck", {
+  expect_identical(data_robins, read.csv("data_raw/data_robins.csv"))
+  expect_identical(data_rob2, read.csv("data_raw/data_rob2.csv"))
+  expect_identical(data_quadas, read.csv("data_raw/data_quadas.csv"))
+  expect_identical(data_rob1, read.csv("data_raw/data_rob1.csv"))
+})
+
--- tests/testthat/test_errors.R
+++ tests/testthat/test_errors.R
@@ -1,6 +1,46 @@
 context("Check error messages")
 
 # Number of columns
+
+
+
+
 # Number of judgements
 # Tool must be specified
+
+test_that("Tool specified", {
+          # expect_error(rob_summary(data_rob2))
+          expect_error(rob_traffic_light(data_rob2))
+          })
+
+test_that("Tool case", {
+  expect_error(rob_summary(data_rob2, "Rob2"))
+  expect_error(rob_traffic_light(data_rob2, "Rob2"))
+})
+
+
+test_that("Colour errors", {
+          expect_error(rob_summary(data_rob2, "ROB2", colour = c("#FFFFFF")))
+          expect_error(rob_traffic_light(data_rob2,"ROB2", colour = c("#FFFFFF")))
+})
+
+test_sum <- data_rob2[, c(1:7)]
+test_tf <- data_rob2[, c(1:6)]
+
+test_that("Too few columns", {
+  expect_error(rob_summary(test, "ROB2"))
+  expect_error(rob_traffic_light(test, "ROB2"))
+})
+
+test_sum <- data_robins[, c(1:9)]
+test_tf <- data_robins[, c(1:8)]
+
+test_that("Too few columns", {
+  expect_error(rob_summary(test, "ROBINS-I"))
+  expect_error(rob_traffic_light(test, "ROBINS-I"))
+})
+
+
+
+
 
