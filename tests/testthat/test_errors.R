ce1405a1144164b0b644ba9d3f6cbfbe7d7594c9
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2020-01-30 15:27:23 +0000

    New testing set with vdiffr

--- tests/testthat/test_errors.R
+++ tests/testthat/test_errors.R
@@ -1,23 +1,15 @@
 context("Check error messages")
 
-# Number of columns
-
-
-
-
-# Number of judgements
 # Tool must be specified
-
-test_that("Tool specified", {
-          # expect_error(rob_summary(data_rob2))
+test_that("No tool specified", {
+          expect_error(rob_summary(data_rob2))
           expect_error(rob_traffic_light(data_rob2))
           })
 
-test_that("Tool case", {
-  expect_error(rob_summary(data_rob2, "Rob2"))
+test_that("Tool specified incorrectly", {
+  expect_error(rob_summary(data_rob2,"Rob2"))
   expect_error(rob_traffic_light(data_rob2, "Rob2"))
 })
-
 
 test_that("Colour errors", {
           expect_error(rob_summary(data_rob2, "ROB2", colour = c("#FFFFFF")))
@@ -36,11 +28,16 @@ test_sum <- data_robins[, c(1:9)]
 test_tf <- data_robins[, c(1:8)]
 
 test_that("Too few columns", {
-  expect_error(rob_summary(test, "ROBINS-I"))
-  expect_error(rob_traffic_light(test, "ROBINS-I"))
+  expect_error(rob_summary(test_sum, "ROBINS-I"))
+  expect_error(rob_traffic_light(test_tf, "ROBINS-I"))
 })
 
+test_sum <- data_quadas[, c(1:6)]
+test_tf <- data_quadas[, c(1:5)]
 
+test_that("Too few columns", {
+  expect_error(rob_summary(test_sum, "QUADAS-2"))
+  expect_error(rob_traffic_light(test_tf, "QUADAS-2"))
+})
 
-
-
+# Need to add specific test for ROB1
