9b7f5a5573b40dcf8bb0e2ea3f1c9034712daea3
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2020-11-19 12:25:23 +0000

    Allow for forest argument in rob_tools

--- R/rob_tools.R
+++ R/rob_tools.R
@@ -2,22 +2,33 @@
 #' @description rob_tools() will list the risk-of-bias assessment tools for
 #'   which templates already exist within the robvis package. If the assessment
 #'   tool you used does not appear in the list, use the "Generic" template.
+#'
+#' @param forest Show tools supported by the forest/blobbogram functions
+#'
 #' @examples
 #' rob_tools()
 #' @family helper
 #' @export
 
-rob_tools <- function() {
-  tools <- c("ROB2",
-    "ROB2-Cluster",
-    "ROBINS-I",
-    "QUADAS-2",
-    "QUIPS",
-    "Generic"
+rob_tools <- function(forest = FALSE) {
+
+  if (forest) {
+    tools <- c("ROB2",
+               "ROBINS-I")
+  } else {
+    tools <- c("ROB2",
+               "ROB2-Cluster",
+               "ROBINS-I",
+               "QUADAS-2",
+               "QUIPS",
+               "Generic"
     )
-  message(
+      message(
     paste0("Note: the \"ROB2-Cluster\" template is only available ",
            "for the rob_traffic_light() function.")
   )
+  }
+
+
   return(tools)
 }
